37aead41defd1fd6d4800a98f8e942bce796123f
Yvone Sanchez <yvone.sre@gmail.com>
2021-10-13 21:50:52 -0500

    Adds route guards and role guards

--- src/components/ProtectedPage/protected.component.jsx
+++ src/components/ProtectedPage/protected.component.jsx
@@ -5,7 +5,10 @@ function Protected(props) {
     return (
         <section className={styles.Protected}>
             <p>This is a super protected route</p>
-            <img src="https://media1.tenor.com/images/4ef46ec3183c923fb14e0c5d0e7564dc/tenor.gif?itemid=16512354"/>
+            <img
+                alt="a funny gif of a man singing"
+                src="https://media1.tenor.com/images/4ef46ec3183c923fb14e0c5d0e7564dc/tenor.gif?itemid=16512354"
+            />
             <p>There are secrets</p>
         </section>
     );
--- src/index.js
+++ src/index.js
@@ -4,54 +4,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter as Router,
-  Route,
-	Switch,
 } from 'react-router-dom';
 import App from './App';
-import {
-  RegisterForm,
-  ProtectedPage,
-  PublicPage
-} from './components';
+
+import { AuthProvider } from './auth-context';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Route
-        path="/"
-        component={App}
-      />
-      <Switch>
-        <Route
-          exact
-          path="/home"
-          render={() => (
-            <div style={{padding: '2rem'}}>
-              <h1>Welcome to this App</h1>
-            </div>
-          )}
-        />
-        <Route
-          exact
-          path="/register"
-          component={RegisterForm}
-        />
-        <Route
-          exact
-          path="/login"
-          component={RegisterForm}
-        />
-        <Route
-          exact
-          path="/protected"
-          component={ProtectedPage}
-        />
-        <Route
-          exact
-          path="/public"
-          component={PublicPage}
-        />
-      </Switch>
+      <AuthProvider>
+        <App/>
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
